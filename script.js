4fee4671167281ab724ef5838208d9175f1bcfe3
Dobrunia <dobkos@hotmail.com>
2023-03-12 18:26:07 +0300

    wf

--- script.js
+++ script.js
@@ -25,3 +25,27 @@ function getStatus(isBusy) {
   var msg = isBusy ? "busy" : "available";
   return { status: msg };
 }
+
+//Sum of two lowest positive integers
+function sumTwoSmallestNumbers(numbers) {
+
+  function selectionSort(array) {
+      for (let i = 0; i < array.length; i++) {
+          let indexMin = i
+          for (let j = i+1; j < array.length; j++) {
+              if (array[j] < array[indexMin]) {
+                  indexMin = j
+              }
+          }
+          let tmp = array[i]
+          array[i] = array[indexMin]
+          array[indexMin] = tmp
+      }
+      return array
+  }
+  let pupupu = selectionSort(numbers);
+    
+    return pupupu[0] + pupupu[1];
+  }
+
+//
